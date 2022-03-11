7b791eb5a890a474be8c0ef6c0496e0ad4655b6a
Maxim Orlov <m.orlov@postgrespro.ru>
2022-03-11 11:35:16 +0300

    README.XID64
    Authors:
    - Alexander Korotkov <aekorotkov@gmail.com>
    - Teodor Sigaev <teodor@sigaev.ru>
    - Nikita Glukhov <n.gluhov@postgrespro.ru>
    - Maxim Orlov <orlovmg@gmail.com>
    - Pavel Borisov <pashkin.elfe@gmail.com>
    - Yura Sokolov <y.sokolov@postgrespro.ru> <funny.falcon@gmail.com>
    Discussion: https://postgr.es/m/CACG%3DezZe1NQSCnfHOr78AtAZxJZeCvxrts0ygrxYwe%3DpyyjVWA%40mail.gmail.com
    Discussion: https://postgr.es/m/CAJ7c6TPDOYBYrnCAeyndkBktO0WG2xSdYduTF0nxq%2BvfkmTF5Q%40mail.gmail.com

--- src/backend/access/transam/clog.c
+++ src/backend/access/transam/clog.c
@@ -845,7 +845,7 @@ BootStrapCLOG(void)
 	SimpleLruWritePage(XactCtl, slotno);
 	Assert(!XactCtl->shared->page_dirty[slotno]);
 
-	pageno = TransactionIdToPage(XidFromFullTransactionId(ShmemVariableCache->nextXid));
+	pageno = TransactionIdToPage(XidFromFullTransactionId(TransamVariables->nextXid));
 	if (pageno != 0)
 	{
 		LWLock *nextlock = SimpleLruGetBankLock(XactCtl, pageno);
