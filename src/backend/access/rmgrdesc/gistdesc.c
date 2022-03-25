8f0f14ef423894435219768cff489383ee27d1fc
Maxim Orlov <m.orlov@postgrespro.ru>
2022-03-25 12:36:24 +0300

    Use 64-bit FullTransactionId instead of Epoch:xid
    NextXid in controldata is now compatible with old format Epoch:xid and the new
    one. This is next step to make XIDs 64-bit.
    
    Author: Alexander Korotkov <aekorotkov@gmail.com>
    Author: Teodor Sigaev <teodor@sigaev.ru>
    Author: Nikita Glukhov <n.gluhov@postgrespro.ru>
    Author: Maxim Orlov <orlovmg@gmail.com>
    Author: Pavel Borisov <pashkin.elfe@gmail.com>
    Author: Yura Sokolov <y.sokolov@postgrespro.ru> <funny.falcon@gmail.com>
    Author: Aleksander Alekseev <aleksander@timescale.com>
    Reviewed-by: Aleksander Alekseev <aleksander@timescale.com>
    Discussion: https://postgr.es/m/CACG%3DezZe1NQSCnfHOr78AtAZxJZeCvxrts0ygrxYwe%3DpyyjVWA%40mail.gmail.com
    Discussion: https://postgr.es/m/CAJ7c6TPDOYBYrnCAeyndkBktO0WG2xSdYduTF0nxq%2BvfkmTF5Q%40mail.gmail.com

--- src/backend/access/rmgrdesc/gistdesc.c
+++ src/backend/access/rmgrdesc/gistdesc.c
@@ -26,11 +26,10 @@ out_gistxlogPageUpdate(StringInfo buf, gistxlogPageUpdate *xlrec)
 static void
 out_gistxlogPageReuse(StringInfo buf, gistxlogPageReuse *xlrec)
 {
-	appendStringInfo(buf, "rel %u/%u/%u; blk %u; snapshotConflictHorizon %u:%llu",
+	appendStringInfo(buf, "rel %u/%u/%u; blk %u; snapshotConflictHorizon %llu",
 					 xlrec->locator.spcOid, xlrec->locator.dbOid,
 					 xlrec->locator.relNumber, xlrec->block,
-					 EpochFromFullTransactionId(xlrec->snapshotConflictHorizon),
-					 (unsigned long long) XidFromFullTransactionId(xlrec->snapshotConflictHorizon));
+					 (unsigned long long) U64FromFullTransactionId(xlrec->snapshotConflictHorizon));
 }
 
 static void
@@ -51,9 +50,8 @@ out_gistxlogPageSplit(StringInfo buf, gistxlogPageSplit *xlrec)
 static void
 out_gistxlogPageDelete(StringInfo buf, gistxlogPageDelete *xlrec)
 {
-	appendStringInfo(buf, "deleteXid %u:%llu; downlink %u",
-					 EpochFromFullTransactionId(xlrec->deleteXid),
-					 (unsigned long long) XidFromFullTransactionId(xlrec->deleteXid),
+	appendStringInfo(buf, "deleteXid %llu; downlink %u",
+					 (unsigned long long) U64FromFullTransactionId(xlrec->deleteXid),
 					 xlrec->downlinkOffset);
 }
 
