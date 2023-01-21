84316597ff2197758f79caa9b2e6807b7948dad3
Andres Freund <andres@anarazel.de>
2023-01-20 21:16:47 -0800

    Zero initialize uses of instr_time about to trigger compiler warnings
    These are all not necessary from a correctness POV. However, in the near
    future instr_time will be simplified to an int64, at which point gcc would
    otherwise start to warn about the changed places.
    
    Reviewed-by: Tom Lane <tgl@sss.pgh.pa.us>
    Discussion: https://postgr.es/m/20230116023639.rn36vf6ajqmfciua@awork3.anarazel.de

--- src/backend/storage/buffer/bufmgr.c
+++ src/backend/storage/buffer/bufmgr.c
@@ -1020,6 +1020,8 @@ ReadBuffer_common(Relation reln,
 
 			if (track_io_timing)
 				INSTR_TIME_SET_CURRENT(io_start);
+			else
+				INSTR_TIME_SET_ZERO(io_start);
 
 			smgrread(smgr, forkNum, blockNum, (char *) bufBlock);
 
@@ -2929,6 +2931,8 @@ FlushBuffer(BufferDesc *buf, SMgrRelation reln)
 
 	if (track_io_timing)
 		INSTR_TIME_SET_CURRENT(io_start);
+	else
+		INSTR_TIME_SET_ZERO(io_start);
 
 	/*
 	 * bufToWrite is either the shared buffer or a copy, as appropriate.
